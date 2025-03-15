f2ed526effff2608c6692ec8585dcfe9280e07b5
randika <randhikamadushan@gmail.com>
2025-03-15 15:57:10 +0000

    Expence Model Created

--- obj/Debug/net9.0/FinanceApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/FinanceApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+965ff2308094df7724d641292f9aa248763852e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
